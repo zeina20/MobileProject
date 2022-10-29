e005cee51d11c759d9764624622535a09ea9e811
nataliee20 <natalie1901991@miuegypt.edu.eg>
2022-10-30 01:47:21 +0200

    adding cinema

--- lib/Cinema.dart
+++ lib/Cinema.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
 
+
+import 'Login.dart';
+import 'SignUp.dart';
+
 class CinemaPage1 extends StatelessWidget {
   const CinemaPage1({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(),
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const SizedBox(
+              height: 85,
+             child: DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Menu'),
+            ),
+            ),
+            ListTile(
+              title: const Text('SignIn'),
+              onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(builder: (context) => LoginPage()));
+                        },
+            ),
+            ListTile(
+              title: const Text('SignUp'),
+               onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(builder: (context) => SignUp()));
+                        },
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(),
       body: SingleChildScrollView(
           child: Container(
@@ -15,6 +47,12 @@ class CinemaPage1 extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (_) => LoginPage()));
+              },
+          child: 
              Stack(children: [
                 Container(
                   height: 200,
@@ -24,28 +62,96 @@ class CinemaPage1 extends StatelessWidget {
                       image: DecorationImage(
                           fit: BoxFit.fill,
                           image: AssetImage(
-                              "assets/images/a.jpg"))),
+                              "assets/images/p90.jfif"))),
                   padding: const EdgeInsets.all(8),
                 ),
-                Positioned(top: 50, left: 120,
+                Positioned(top: 70, left: 145,
                   child: Text(
-                    "data",
-                    style: TextStyle(color: Colors.white, fontSize: 20),
+                    "Point 90",
+                    style: TextStyle(color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),
                   ),
-                )
+                ),
+                
               ]),
+            ),
+               Divider(),
+              Stack(children: [
+                Container(
+                  height: 200,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: const BoxDecoration(
+                      color: Colors.grey,
+                      image: DecorationImage(
+                          fit: BoxFit.fill,
+                          image: AssetImage(
+                              "assets/images/center.jfif"))),
+                  padding: const EdgeInsets.all(8),
+                ),
+                Positioned(top: 70, left: 125,
+                  child: Text(
+                    "City Center",
+                    style: TextStyle(color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                
+              ]),
+
+          //   Divider(),
+          //   Image.asset("assets/images/a.jpg"),
+          //   Divider(),
+          //   Image.asset("assets/images/a.jpg"),
+          //   Divider(),
+          //   Image.asset("assets/images/a.jpg"),
+          //   Divider(),
+          //   Image.asset("assets/images/a.jpg"),
+          //   Divider(),
+          //   Image.asset("assets/images/a.jpg"),
+          //   Divider(),
+          //   Image.asset("assets/images/a.jpg"),
             Divider(),
-            Image.asset("assets/images/a.jpg"),
-            Divider(),
-            Image.asset("assets/images/a.jpg"),
-            Divider(),
-            Image.asset("assets/images/a.jpg"),
-            Divider(),
-            Image.asset("assets/images/a.jpg"),
-            Divider(),
-            Image.asset("Assets/Photos/panel-gd-300x150.jpg"),
-            Divider(),
-            Image.asset("Assets/Photos/panel-gd-300x150.jpg"),
+              Stack(children: [
+                Container(
+                  height: 200,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: const BoxDecoration(
+                      color: Colors.grey,
+                      image: DecorationImage(
+                          fit: BoxFit.fill,
+                          image: AssetImage(
+                              "assets/images/cairo.jfif"))),
+                  padding: const EdgeInsets.all(8),
+                ),
+                Positioned(top: 70, left: 115,
+                  child: Text(
+                    "Cairo Festival ",
+                    style: TextStyle(color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+              ),
+                 ]),
+
+
+              Divider(),
+              Stack(children: [
+                Container(
+                  height: 200,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: const BoxDecoration(
+                      color: Colors.grey,
+                      image: DecorationImage(
+                          fit: BoxFit.fill,
+                          image: AssetImage(
+                              "assets/images/arabia.jpg"))),
+                  padding: const EdgeInsets.all(8),
+                ),
+                Positioned(top: 70, left: 115,
+                  child: Text(
+                    "Mall Of Arabia",
+                    style: TextStyle(color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                
+              ]),
+              
           ],
         ),
       )),
