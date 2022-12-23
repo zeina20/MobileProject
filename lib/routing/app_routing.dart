a932586ba195318ca3d71e922d10a1317a2c4a57
Marwann1 <marwanmaged1962@gmail.com>
2022-12-23 23:34:21 +0200

    routing comment

--- lib/routing/app_routing.dart
+++ lib/routing/app_routing.dart
@@ -1,32 +1,32 @@
-import 'package:MobileProject/view/SignUp.dart';
-import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+// import 'package:MobileProject/view/SignUp.dart';
+// import 'package:flutter/material.dart';
+// import 'package:go_router/go_router.dart';
 
-import '../view/Cinema.dart';
-import '../view/Films.dart';
-import '../view/Login.dart';
+// import '../view/Cinema.dart';
+// import '../view/Films.dart';
+// import '../view/Login.dart';
 
-final GoRouter router = GoRouter(
-  routes: <GoRoute>[
-    GoRoute(
-      path: '/',
-      builder: (BuildContext context, GoRouterState state) => CinemaPage1(),
+// final GoRouter router = GoRouter(
+//   routes: <GoRoute>[
+//     GoRoute(
+//       path: '/',
+//       builder: (BuildContext context, GoRouterState state) => CinemaPage1(),
       
       
-    ),
+//     ),
        
-    GoRoute(
-      path: '/loginpage',
-      builder: (BuildContext context, GoRouterState state) => LoginPage(),
+//     GoRoute(
+//       path: '/loginpage',
+//       builder: (BuildContext context, GoRouterState state) => LoginPage(),
       
       
-    ),
-    GoRoute(
-      path: '/signup',
-      builder: (BuildContext context, GoRouterState state) => SignUp(),
+//     ),
+//     GoRoute(
+//       path: '/signup',
+//       builder: (BuildContext context, GoRouterState state) => SignUp(),
       
       
-    ),
+//     ),
     
-  ]
-);
+//   ]
+// );
