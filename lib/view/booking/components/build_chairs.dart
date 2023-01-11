4aeb17de896cf2d4a214ee57dfa395d11f479af0
nataliee20 <natalie1901991@miuegypt.edu.eg>
2023-01-11 02:26:13 +0200

    chairs

---
+++ lib/view/booking/components/build_chairs.dart
@@ -0,0 +1,37 @@
+import 'package:MobileProject/constants/constants.dart';
+import 'package:flutter/material.dart';
+
+class BuildChairs{
+  static Widget selectedChair(){
+    return Container(
+      height: 10.0,
+      width: 10.0,
+      decoration: BoxDecoration(
+        color: primary,
+        borderRadius: BorderRadius.circular(6.0)
+      ),
+    );
+  }
+
+  static Widget availableChair(){
+    return Container(
+      height: 10.0,
+      width: 10.0,
+      decoration: BoxDecoration(
+          border: Border.all(color: white),
+          borderRadius: BorderRadius.circular(6.0)
+      ),
+    );
+  }
+
+  static Widget reservedChair(){
+    return Container(
+      height: 10.0,
+      width: 10.0,
+      decoration: BoxDecoration(
+          color: white,
+          borderRadius: BorderRadius.circular(6.0)
+      ),
+    );
+  }
+}
